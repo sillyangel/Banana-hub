55c8a9d44a072ca71f2f3c7b34c938a6ba3f62c9
SIllyangel3 <50461810+banana-nana@users.noreply.github.com>
2022-01-05 07:52:02 -0600

    update

--- main.lua
+++ main.lua
@@ -1,7 +1,6 @@
 if game.PlaceId == 155615604 then
-    print("Loading Prison Life beta...")
     local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-    local Window = Library.CreateLib("Prison Life", "Sentinel")
+    local Window = Library.CreateLib("Prison Life", "Midnight")
     
     local Main = Window:NewTab("Main")
     local MainS = Main:NewSection("Main")
@@ -11,6 +10,25 @@ if game.PlaceId == 155615604 then
         Event:InvokeServer(A_1)
     end)
     
+    MainS:NewDropdown("Gun Mod", "makes the gun op", {"M9", "Remington 870", "AK-47"}, function(v)
+        local module = nil
+        if game:GetService("Players").LocalPlayer.Backpack:FindFirstChild(v) then
+            module = require(game:GetService("Players").LocalPlayer.Backpack[v].FindFirstChild(v).GunStates)
+        elseif game:GetService("Players").LocalPlayer.Character:FindFirstChild(v) then
+            module = require(game:GetService("Players").LocalPlayer.Character[v].GunStates)   
+        end
+        if module ~= nil then
+            module["MaxAmmo"] = math.huge
+            module["CurrentAmmo"] = math.huge
+            module["StoredAmmo"] = math.huge
+            module["FireRate"] = 0.000001
+            module["Spread"] = 0
+            module["Range"] = math.huge
+            module["Bullets"] = 10
+            module["ReloadTime"] = 0.000001
+            module["AutoFire"] = true
+        end
+    end)
     local Player = Window:NewTab("Player")
     local PlayerS = Player:NewSection("Player")
 
@@ -22,7 +40,7 @@ if game.PlaceId == 155615604 then
     end)
 
 else
-    print("this is not a sppored game")
+    print("This game is not Supported")
 end
 
 
