1ffcc9d50ee315792bd8804415f6675cc73194b0
SIllyangel3 <50461810+banana-nana@users.noreply.github.com>
2022-01-04 18:51:19 -0600

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,31 @@
+if game.PlaceId == 155615604 then
+    local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+    local Window = Library.CreateLib("Prison Life", "Sentinel")
+    
+    local Main = Window:NewTab("Main")
+    local MainS = Main:NewSection("Main")
+    MainS:NewDropdown("Give Gun", "Give you a gun", {"M9", "Remington 870", "AK-47"}, function(v)
+        local A_1 = game:GetService("Workspace")["Prison_ITEMS"].giver[v].ITEMPICKUP
+        local Event = game:GetService("Workspace").Remote.ItemHandler
+        Event:InvokeServer(A_1)
+    end)
+    
+    local Player = Window:NewTab("Player")
+    local PlayerS = Player:NewSection("Player")
+
+    PlayerS:NewSlider("WalkSpeed", "Changes The Speed", 250, 0, function(v) -- 500 (MaxValue) | 0 (MinValue)
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = v
+    end)
+    PlayerS:NewSlider("JumpPower", "Changes The JumpPower", 250, 0, function(v) -- 500 (MaxValue) | 0 (MinValue)
+        game.Players.LocalPlayer.Character.Humanoid.JumpPower = v
+    end)
+
+
+end
+
+
+
+
+
+
+
